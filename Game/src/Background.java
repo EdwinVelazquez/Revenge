5108fa743da211bc3b10559e358747f30c382772
Josue Rojas <josuerojas.rojas@gmail.com>
2016-05-11 15:22:10 -0400

    Update Background.java

--- Game/src/Background.java
+++ Game/src/Background.java
@@ -1,9 +1,16 @@
-
+/**
+ * this class is the background for the introduction and select screen
+ * @author josuerojas
+ *
+ */
 public class Background extends Animation{
 
 	public Background() {
-		super("", 0, 0, "");
-		// TODO Auto-generated constructor stub
+		super("Images/Background/background_",5, 101, ".jpg");
+
+	}
+	public Background(int duration) {
+		super("Images/Background/background_",duration, 101, ".jpg");
 	}
 
 }
