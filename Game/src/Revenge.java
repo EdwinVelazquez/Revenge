9b1815a2dcb7a2e4e3a4506f3524ac387f1599e8
Josue Rojas <josuerojas.rojas@gmail.com>
2016-05-11 15:35:25 -0400

    Update Revenge.java
    it will not display right if your dimension screen is not 1440x900

--- Game/src/Revenge.java
+++ Game/src/Revenge.java
@@ -8,12 +8,13 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JCheckBoxMenuItem; //check box menu
+import javax.swing.JFrame; //window
+import javax.swing.JMenu; //menu
+import javax.swing.JMenuBar; //menu bar
+import javax.swing.JMenuItem; //menu item
+import javax.swing.JRadioButtonMenuItem; //radio menu button
+import javax.swing.JOptionPane;
 
 /**
  * this class is the main game (Revenge)
@@ -28,15 +29,25 @@ public class Revenge extends JFrame implements Runnable {
 	boolean pause = false;
 	boolean start = true;
 	boolean select = false;
+	boolean playerSelect = false;
 	StartScreen startScreen;
+	CreditScreen creditScreen;
+	Background background;
 	//this is used for full screen
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	int fullW = (int) screenSize.getWidth();
-	int fullH = (int) screenSize.getHeight();
+	//int fullW = (int) screenSize.getWidth();
+	//int fullH = (int) screenSize.getHeight();
+	
+	//the images are set up for this dimension
+	int fullW = 1440;
+	int fullH = 900;
 
 	KeyboardControl k = new KeyboardControl();
 	int time;
 	int delay; //delay for button presses
+	
+	String player;
+
 	
 	
 	public static void main(String[] args) {
@@ -54,12 +65,18 @@ public class Revenge extends JFrame implements Runnable {
 		bbg.setColor(Color.WHITE); 
 		bbg.fillRect(0, 0, fullW, fullH);
 		bbg.setColor(Color.black);
-		bbg.drawString("Time: " + time, 20, 40);
 		if(start){
-			if(select){
-				//selectScreen.draw() should go here.
-				//this screen should be where the credits are given. or the info
-				bbg.drawString("Press select", 20, 60);
+			background.draw(bbg, 0, 0);
+			if(select){ //this is the select screen (setting/credit/info screen)
+				creditScreen.draw(bbg); //display credits
+					if(playerSelect){ //this should be handled somewhere else
+						player = JOptionPane.showInputDialog("Current Player: " + player);
+						if(player == null || player.equals("")){ //avoid empty box
+							player = "Player1";
+						}
+						playerSelect = false;
+						k.Start = false; //there is a bug here where it gets stuck in true (will fix later for now this is quick fix)
+					}
 			}
 			else{
 				bbg.drawString("Press not select", 20, 60);
@@ -69,24 +86,34 @@ public class Revenge extends JFrame implements Runnable {
 		else{
 			//start game here
 		}
+		bbg.drawString("Time: " + time, 20, 40);
 		g.drawImage(backBuffer, 0, 0, this);
 		
 		
 
 	}
 	
+	/**
+	 * this method handles all the update stuff
+	 */
 	public void update() {
 		time++;
 		delay++;
 		//change the screen 
-		if(k.pressStart()){
+		if(select && k.pressStart() && delay > 30){
+			playerSelect = true;
+			delay = 0;
+		}
+		else if(k.pressStart() && delay > 30){
 			start = false;
+			delay = 0;
 		}
 		//if it is the start screen and press select when the delay is greater than 30
 		if(start && k.pressSelect() && delay > 30){
 			select = !select;
 			delay = 0;
 		}
+	
 	}
 
 	/**
@@ -103,11 +130,15 @@ public class Revenge extends JFrame implements Runnable {
 	    backBuffer = new BufferedImage(fullW, fullH, BufferedImage.TYPE_INT_RGB); 
 	    //initialize screens
 	    startScreen = new StartScreen(fullW,fullH);
+	    creditScreen = new CreditScreen(fullW,fullH);
+	    background = new Background();
 	    setDefaultCloseOperation(EXIT_ON_CLOSE); 
 	    setVisible(true); 
+	    Camera.setLocation(200, 0);
 	    //time is a clock for keeping track of delays etc
 	    time = 0;
 	    delay = 0;
+	    player = "Player1"; //starts with default player 'Player1'
 	
 	}
 
