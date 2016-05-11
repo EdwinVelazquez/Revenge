32f534ab0c2ecf2b3e4237177532ce2cea08a4df
Josue Rojas <josuerojas.rojas@gmail.com>
2016-05-11 15:27:50 -0400

    Add files via upload

---
+++ Game/src/Credit.java
@@ -0,0 +1,14 @@
+/**
+ * this represents a screen in this case it is the beggining when you press select 
+ * it is a credit screen which includes player scores, controls, and maybe settings if i have time
+ * @author josuerojas
+ *
+ */
+public class Credit extends Animation{
+
+	public Credit() {
+		super("Images/CreditScreen/credit_", 10, 1, ".png");
+		// TODO Auto-generated constructor stub
+	}
+
+}
