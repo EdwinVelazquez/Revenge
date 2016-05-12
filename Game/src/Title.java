1a1dcee70dd9a7a5b006a0ce38f14fb18d1f66bd
Josue Rojas <josuerojas.rojas@gmail.com>
2016-05-12 18:50:13 -0400

    Update Title.java
    can change dimensions

--- Game/src/Title.java
+++ Game/src/Title.java
@@ -8,6 +8,9 @@ public class Title extends Animation{
 	public Title() {
 		super("Images/StartScreen/title_", 15, 6, ".png");
 	}
+	public Title(int w, int h) {
+		super("Images/StartScreen/title_", 15, 6, ".png",w,h);
+	}
 
 	
 }
