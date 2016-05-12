21138cb2738dea5e7ed9319786708468b9ae1c21
Josue Rojas <josuerojas.rojas@gmail.com>
2016-05-12 18:49:52 -0400

    Update StartScreen.java

--- Game/src/StartScreen.java
+++ Game/src/StartScreen.java
@@ -7,16 +7,18 @@ import java.awt.Graphics;
  */
 public class StartScreen {
 	
-	static Title t = new Title();
-	static Background b = new Background();
-	static int width;
-	static int height;
+	//static Background b = new Background();
+	static int width = 1440;
+	static int height = 900;
+	Title t;
 	public StartScreen(int width, int height){
 		this.width = width;
 		this.height = height;
+		t = new Title(width, height);
 
 	}
 	public void draw(Graphics g){
-		t.draw(g, 0,0); //needs to be fix to calculate the middle for different dimensions
+	//	b.draw(g, 0, 0);
+		t.draw(g, 0,-60); //needs to be fix to calculate the middle for different dimension
 	}
 }
