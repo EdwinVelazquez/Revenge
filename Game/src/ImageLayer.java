58f9d19c58413a692c226b6f1ea0ae159a6cfeb3
Josue Rojas <josuerojas.rojas@gmail.com>
2016-05-17 15:32:01 -0400

    Update ImageLayer.java
    added infinite parallex scrolling to the right
    still having problems with the left and flickering

--- Game/src/ImageLayer.java
+++ Game/src/ImageLayer.java
@@ -11,18 +11,27 @@ public class ImageLayer
    private float y = 0;
 
    private float d = 1;
+   private int endW;
+   String filename;
 
 
    public ImageLayer(String filename)
    {
 	  image = Toolkit.getDefaultToolkit().getImage(filename);
+	  this.filename = filename;
+	  endW =  this.getWidth();
+	  System.out.println(endW);
    }
 
    public ImageLayer(String filename, float distance)
    {
 	  image = Toolkit.getDefaultToolkit().getImage(filename);
 
+	  this.filename = filename;
       setDistance(distance);
+      //return -1 because it cannot get the width yet
+	  endW = this.getWidth();
+	  System.out.println(endW);
    }
 
    public void setX(float x)
@@ -40,12 +49,47 @@ public class ImageLayer
       d = distance;
    }
 
-   public void draw(Graphics g)
+   public int getWidth(){
+	   return image.getWidth(null);
+   }
+   public void draw(Graphics g){
+	   for(int i = 0; i < 5; i++)
+	      {
+	         g.drawImage(image, i*image.getWidth(null) +(int)(x - Camera.x / d), (int)(y - Camera.y / d), null);
+	      }
+   }
+   /**
+    * NEEDS TO BE FIX FOR FLICKERING AND NOT MOVING LEFT
+    * @param g
+    */
+   public void drawInfinite(Graphics g)
    {
 
-      for(int i = 0; i < 5; i++)
+	   this.endW =  this.getWidth();
+      for(int i = 0; i < 3; i++)
       {
-         g.drawImage(image, i*image.getWidth(null) +(int)(x - Camera.x / d), (int)(y - Camera.y / d), null);
+    	  int pointScreenX =  i*image.getWidth(null) +(int)(x - Camera.x / d);
+    	  int endWScreenX = endW + (int)(x - Camera.x / d);
+         g.drawImage(image,pointScreenX, (int)(y - Camera.y / d), null);
+         
+         if(pointScreenX < -endW){
+        	// int offset = -endW - point screen
+        	 //drew it on top of the first
+        	 //there is an offset that causes the flickering
+        	 
+        	 x += endW;
+         }
+         /*
+         //creating it for the opposite direction makes it flicker because of the contradiction
+         else {
+         	// int offset = -endW - point screen
+         	 //drew it on top of the first
+         	 //there is an offset that causes the flickering
+         	 x -= endW;
+          }
+          */
+         System.out.println(pointScreenX + " " + filename + " width  = " + image.getWidth(null));
+         
       }
    }
 }
